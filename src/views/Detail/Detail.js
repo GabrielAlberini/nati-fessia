71df32f83033c4592e2dfc71205dc82dffd45c8e
CuerpoCasa <gabialberini733@gmail.com>
2022-04-11 16:16:22 -0300

    V2.7

--- src/views/Detail/Detail.js
+++ src/views/Detail/Detail.js
@@ -25,17 +25,19 @@ const Detail = ({
       }}
     >
       <section className="modal-detail">
-        <div className="close-detail" onClick={() => {
+        <div
+          className="close-detail"
+          onClick={() => {
             setIsModalEditar(false);
             setProductoEditar(null);
-          }}>
+          }}
+        >
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
             <path d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z" />
           </svg>
         </div>
         <h2 className="title-detail">{productoEstado.cliente}</h2>
         <h5>{productoEstado.descripcion}</h5>
-        <a href={productoEstado.web} rel="noreferrer" target='_blank'><h5>{productoEstado.web}</h5></a>
         <article>
           <div className="main-img-detail">
             {productoEstado.foto1 && (
@@ -93,6 +95,13 @@ const Detail = ({
               </div>
             )}
           </div>
+          <a
+            href={`https://${productoEstado.web}`}
+            rel="noreferrer"
+            target="_blank"
+          >
+            {productoEstado.web}
+          </a>
           <h5 className="title-detail">{productoEstado.categoria}</h5>
           <span className="title-detail">{productoEstado.hashtag}</span>
         </article>
